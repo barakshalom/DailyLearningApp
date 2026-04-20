99b78aac17304b6c6a9c361c06ec9e2fdae41020
barakshalom <barak@idus.co.il>
2026-04-20 14:16:01 +0300

    Implemented very basic lessons switcher

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script lang="ts">
+    let LessonsList = ["Lesson 1", "Lesson 2", "Lesson 3", "Lesson 4", "Lesson 5", "Lesson 6", "Lesson 7", "Lesson 8", "Lesson 9", "Lesson 10"];
+
+    let currentLesson = $state(LessonsList[0]);
+
+    function SwitchLesson() {
+        currentLesson = LessonsList[Math.floor(Math.random() * LessonsList.length)];
+    }
+</script>
+
+<h1>{currentLesson}</h1>
+
+<button onclick={() => SwitchLesson()}>Switch Lesson</button>
